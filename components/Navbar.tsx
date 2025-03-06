4b6fa5714bf7f251fc1cdfb91ff4125880385a4a
Mohammed Abdelsalhyen <114920462+mohammedsalhyen@users.noreply.github.com>
2025-03-06 15:13:53 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav>
             <div className=' container max-container mx-auto py-5 '>
-                <div className=' w-full padding-container 2xl:px-0  flex justify-between  '>
+                <div className=' w-full padding-container 2xl:px-0  flex items-center justify-between  '>
                     <div className='image-container md:w-36 xs:w-28 '>
                         <Link href="/">
                             <img src={"/asset/logo.webp"} className=' w-full' alt='Mohammed' loading='lazy' />
