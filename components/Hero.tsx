60861113e116668e12ee804f26d19ff66ce5beff
Mohammed Abdelsalhyen <114920462+mohammedsalhyen@users.noreply.github.com>
2025-03-06 15:05:17 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
         { href: "https://wa.me/201110948994", icon: <FaWhatsapp />, text: "Whatsapp" }
     ];
     return (
-        <main className=' padding-container max-container main-prop w-full border-b-2 border-[var(--border)]'>
+        <main className=' padding-container max-container w-full border-b-2 border-[var(--border)]'>
             <div className=' flex  xs:flex-col lg:flex-row  justify-between   '>
                 <div className=' flex-1 relative overflow-hidden xs:text-center lg:text-start'>
                     <div>
